e99cb36696fb62aae24a79f54684e63e9be83f65
victor.zou <victor.zou@ef.com>
2014-08-19 16:54:48 +0800

    Adding webapi as the first owin middleware.

---
+++ src/GreenField.API/Startup.cs
@@ -0,0 +1,24 @@
+﻿using GreenField.API.App_Start;
+using Microsoft.Owin;
+using Owin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+
+//[assembly: OwinStartup(typeof(GreenField.API.Startup))]
+
+namespace GreenField.API
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            var config = new HttpConfiguration();
+
+            WebApiConfig.Register(config);
+            app.UseWebApi(config);
+        }
+    }
+}
