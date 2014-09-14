bff1b979097e917e45f1ac9c6de37182016714d7
victor <supertigerzou@gmail.com>
2014-09-14 21:47:11 +0800

    seed book data

--- src/GreenField.API/Modules/GreenField.Books/Migrations/Configuration.cs
+++ src/GreenField.API/Modules/GreenField.Books/Migrations/Configuration.cs
@@ -1,3 +1,6 @@
+using System;
+using System.Linq;
+using GreenField.Books.Data.DomainModels;
 using GreenField.Framework.Data.DomainModels;
 using System.Data.Entity.Migrations;
 
@@ -15,19 +18,39 @@ namespace GreenField.Books.Migrations
             //  This method will be called after migrating to the latest version.
 
             //  Use the DbSet<T>.AddOrUpdate() helper extension method to avoid creating duplicate seed data.
+            var userVictor = new ApplicationUser
+            {
+                UserName = "victor.zou",
+                PasswordHash = "ADWj64qPNVxOr988AtL7WKaHKkOYSP9LFWUQniZIRxnXFaNJHELTF4kp+FtTnrYe6Q=="
+            };
+
             context.Users.AddOrUpdate(
               p => p.UserName,
-              new ApplicationUser
-              {
-                  UserName = "victor.zou",
-                  PasswordHash = "ADWj64qPNVxOr988AtL7WKaHKkOYSP9LFWUQniZIRxnXFaNJHELTF4kp+FtTnrYe6Q=="
-              },
+              userVictor,
               new ApplicationUser
               {
                   UserName = "unique.lin",
                   PasswordHash = "ADWj64qPNVxOr988AtL7WKaHKkOYSP9LFWUQniZIRxnXFaNJHELTF4kp+FtTnrYe6Q=="
               }
             );
+
+            context.SaveChanges();
+
+            context.Authors.AddOrUpdate(
+                p => p.Id,
+                new Author
+                {
+                    FirstName = "victor",
+                    LastName = "zou",
+                    ModifiedDate = DateTime.Now,
+                    LoginUser = userVictor
+                },
+                new Author
+                {
+                    FirstName = "unique",
+                    LastName = "lin",
+                    ModifiedDate = DateTime.Now
+                });
         }
     }
 }
